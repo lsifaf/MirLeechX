a293220ec916049bd05a5e8d2ddfd2a9a7f42fe9
ichigooe <94220399+ichigooe@users.noreply.github.com>
2021-11-24 15:46:39 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -45,7 +45,7 @@ def stats(update, context):
 
 def start(update, context):
     buttons = button_build.ButtonMaker()
-    buttons.buildbutton("Happy Mirroring", "https://google.com")
+    buttons.buildbutton("Join For Updates", "https://t.me/jkxmirror")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
