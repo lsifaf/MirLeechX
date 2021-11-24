4487be532b97505e08ca2829b0cb117129332014
ichigooe <94220399+ichigooe@users.noreply.github.com>
2021-11-24 15:42:56 +0530

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -577,9 +577,9 @@ class GoogleDriveHelper:
                     content += f'<b> | <a href="https://telegra.ph/{self.path[nxt_page]}">Next</a></b>'
                     nxt_page += 1
             Telegraph(access_token=telegraph_token).edit_page(path = self.path[prev_page],
-                                 title = 'MirLeechX Search',
-                                 author_name='MirLeechX',
-                                 author_url='https://github.com/iamLiquidX/MirLeechX',
+                                 title = 'JKxtorrents Search',
+                                 author_name='JKxTorrents',
+                                 author_url='https://google.com',
                                  html_content=content)
         return
 
